535767d1fe1f7b9fe3f8b0b2912acf7702c1a9c1
rodolfofadino <rodolfo.fadino@gmail.com>
2022-10-27 14:26:03 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fiap2022.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //public JsonResult Index()
+        //public ActionResult Index()
+        //public ActionResult<Pessoa> Index()
+        //public Pessoa Index()
+        //public IActionResult Index()
+        //public ViewResult Index()
+        public ViewResult Index()
+        {
+            ViewData["nome"] = "Gabriel";
+            ViewData["pessos"] = new Pessoa() { Nome = "Luiz" };
+            //TempData["TesteTempData"] = "Teste";// new Pessoa() { Nome = "Roberto" };
+            ViewBag.Categoria = "Alimentos";
+
+
+
+            var viewModel = new HomeViewModel()
+            {
+                Pessoas = new List<Pessoa>(),
+                Paises = new List<Pais>()
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                viewModel.Pessoas.Add(new Pessoa() { Nome = $"Ana {i + 1}" });
+                viewModel.Paises.Add(new Pais() { Nome = $"Brasil {i + 1}" });
+            }
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+//builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "customizada",
+    defaults: new { controller = "Home", action = "Index" },
+    pattern: "minha-area/{id?}"
+//pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+
+app.MapControllerRoute(
+    name: "default",
+    defaults: new {controller="Home", action="Index" },
+    pattern: "{controller}/{action}/{id?}"
+    //pattern: "{controller=Home}/{action=Index}/{id?}"
+); 
+
+app.Run();
+
+
+
+
+
+
+
+//app.MapGet("/", () =>
+//{
+//    var pessoa = new Pessoa() { Nome="Fabio"};
+//    return pessoa;
+//});
+
+[Serializable]
+public class Pessoa
+{
+    public string Nome { get; set; }
+}
+
+public class Pais
+{
+    public string Nome { get; set; }
+}
+
+
+public class HomeViewModel
+{
+    //public Pais PaisSelecionado { get; set; }
+    public List<Pais> Paises { get; set; }
+    public List<Pessoa> Pessoas { get; set; }
+}
